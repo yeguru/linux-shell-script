f942bff14558cf27016d8d3797cf9c37972cf10e
yeguru <yr947987@gmail.com>
2024-12-31 18:48:11 +0530

    varaible script is added

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1    :: Hi $PERSON2"
+echo "$PERSON2   :: how are you $PERSON1"
+echo "$PERSON1     :: fine $PERSON2 "
+echo "$PERSON2    :: great $PERSON1"
