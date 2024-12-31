651746c0e03e91de66c8a79d649059a2480fc7d6
yeguru <yr947987@gmail.com>
2024-12-31 18:55:38 +0530

    arguements passed through varibale

---
+++ 04.varaiables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1    :: Hi $PERSON2"
+echo "$PERSON2   :: how are you $PERSON1"
+echo "$PERSON1     :: fine $PERSON2 "
+echo "$PERSON2    :: great $PERSON1"
