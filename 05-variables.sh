9c31cc4e2ed4a4e5efe65725da272b8ebcac5b6b
yeguru <yr947987@gmail.com>
2024-12-31 19:16:16 +0530

    arguements username and password  passed through varibale

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter the valid user name:"
-read USERNAME
+read -s USERNAME
 echo "entered  username as :$USERNAME"
 
 echo "Please enter the valid password:"
-read PASSWORD 
+read -s PASSWORD 
 echo "entered  password  as :$PASSWORD"
