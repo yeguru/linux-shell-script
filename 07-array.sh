c59bacfc85e44e435dc2bb149854f9bb7b589505
yeguru <yr947987@gmail.com>
2024-12-31 19:43:13 +0530

    Array list

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+movies=("Devara" "Pushpa" "RRR")
+
+echo "The first movie is:${movies[0]}"
+echo "The second movie is:${movies[1]}"
+echo "The third movie is:${movies[2]}"
+echo "All the  movies are:${movies[@]}"
