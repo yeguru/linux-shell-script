2aaa6cf36cfa781ba633af57d869363faf97e095
yeguru <yr947987@gmail.com>
2025-01-07 11:00:50 +0530

    speciail variables

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Number=$1
+
+if[$Number -gt 99]
+then
+    echo ("The number is greater than 99 :$Number")
+else
+    echo ("The number is less than the 99 or equal to 99: $number")
+fi        
---
+++ 10-install-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if[$userid -ne 0]
+then
+    echo ("ERROR ::The user should have the sudo access to execute the script")
+    exit 127
+fi
+
+dnf install mysql -y
+
+if[$? -ne 0]
+then
+    echo ("The installation of mysql is Failed")
+    exit 127
+else
+    echo ("The installation of mysql is sucess")
+fi        
+
+dnf install git -y
+
+if[$? -ne 0]
+then
+    echo ("The installation of git is Failed")
+    exit 127
+else
+    echo ("The installation of git is sucess")
+fi 
