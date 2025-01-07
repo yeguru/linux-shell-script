060fcdc4e45acc78bac394cda134685e8bb291cd
yeguru <yr947987@gmail.com>
2025-01-07 12:19:22 +0530

    apps installing using functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -12,29 +12,29 @@ VALIDATE(){
     fi        
          }
 
-if [ $userid ne 0 ]
-then
-    echo "ERROR: Please execute the script with the root/sudo privileges"
-    exit 127
-fi
+    if [ $userid ne 0 ]
+    then
+        echo "ERROR: Please execute the script with the root/sudo privileges"
+        exit 127
+    fi
 
 dnf list installed  lvm  
 
-if [ $? -ne 0 ]
-then
-    dnf install lvm -y
-    VALIDATE $? "Installing.... lvm"
-else
-    echo "LVM is already ...INSTALLED"
-fi
+    if [ $? -ne 0 ]
+    then
+        dnf install lvm -y
+        VALIDATE $? "Installing.... lvm"
+    else
+        echo "LVM is already ...INSTALLED"
+    fi
 
 dnf list installed java
 
-if [ $? -ne 0 ]
-then
-    dnf install java -y
-    VALIDATE $? "Installing.... java"
-else
-    echo "java is already ...INSTALLED"
-fi    
+    if [ $? -ne 0 ]
+    then
+        dnf install java -y
+        VALIDATE $? "Installing.... java"
+    else
+        echo "java is already ...INSTALLED"
+    fi    
 
