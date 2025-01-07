21cd0fbe9f552caf63fdf622b7442fa4240a9927
yeguru <yr947987@gmail.com>
2025-01-07 11:10:04 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,7 +5,7 @@ Number=$1
 if [$Number -ge 99]
 
 then
-    echo ("The number is greater than 99 :$Number")
+    echo "The number is greater than 99 :$Number"
 else
-    echo ("The number is less than the 99 or equal to 99: $number")
+    echo "The number is less than the 99 or equal to 99: $number"
 fi        
